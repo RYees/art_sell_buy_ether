82eff8263f040b69167ed919b0c2f45d607c1933
rehmet yeshanew <ryfgmz87@gmail.com>
2024-02-04 23:25:10 +0300

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,34 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+const fs = require('fs');
+// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+require('dotenv').config();
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/wWXydMnyyT2sSENkoJFYlzdm2bKT8GTy",
+      accounts: [process.env.PRIVATE_KEY],
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
